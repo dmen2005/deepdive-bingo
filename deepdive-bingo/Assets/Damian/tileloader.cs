2c6b3afa6dda8b5ce2472cedca2028c411640760
Damian <drentdamian@gmail.com>
2025-05-06 11:23:43 +0200

    aded map

---
+++ deepdive-bingo/Assets/Damian/tileloader.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+using System;
+
+
+public class tileloader : MonoBehaviour
+{
+    public Renderer tileRenderer;
+    public int zoom = 15;
+    public double latitude = 53.2194;
+    public double longitude = 6.5665;
+
+    void Start()
+    {
+        int x = LonToTileX(longitude, zoom);
+        int y = LatToTileY(latitude, zoom);
+        string url = $"https://tile.openstreetmap.org/{zoom}/{x}/{y}.png";
+        StartCoroutine(LoadTile(url));
+    }
+
+    IEnumerator LoadTile(string url)
+    {
+        UnityWebRequest www = UnityWebRequestTexture.GetTexture(url);
+        yield return www.SendWebRequest();
+
+        if (www.result != UnityWebRequest.Result.Success)
+            Debug.LogError(www.error);
+        else
+            tileRenderer.material.mainTexture = DownloadHandlerTexture.GetContent(www);
+    }
+
+    int LonToTileX(double lon, int zoom) =>
+        (int)((lon + 180.0) / 360.0 * (1 << zoom));
+
+    int LatToTileY(double lat, int zoom)
+    {
+        double latRad = lat * Math.PI / 180.0;
+        return (int)((1.0 - Math.Log(Math.Tan(latRad) + 1.0 / Math.Cos(latRad)) / Math.PI) / 2.0 * (1 << zoom));
+    }
+}
