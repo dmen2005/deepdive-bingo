0c292fb064e7d3e02b0f607203abab3d6c591cba
Mimicall <srhoekstra@st.noorderpoort.nl>
2025-05-06 13:42:59 +0200

    e

--- deepdive-bingo/Assets/Damian/tileloader.cs
+++ deepdive-bingo/Assets/Damian/tileloader.cs
@@ -9,8 +9,8 @@ public class tileloader : MonoBehaviour
 {
     public Renderer tileRenderer;
     public int zoom = 15;
-    public double latitude = 53.2194;
-    public double longitude = 6.5665;
+    [HideInInspector] public double latitude = 53.2194;
+    [HideInInspector] public double longitude = 6.5665;
 
     public void location()
     {
