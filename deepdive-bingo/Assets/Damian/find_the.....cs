d00ce506141b212c42780c82094215b74992f880
Damian <drentdamian@gmail.com>
2025-05-07 15:14:06 +0200

    smal update

--- deepdive-bingo/Assets/Damian/find_the.....cs
+++ deepdive-bingo/Assets/Damian/find_the.....cs
@@ -1,14 +1,22 @@
+using System.Globalization;
 using UnityEngine;
 
 public class find_the: MonoBehaviour
 {
     public GameObject findthemistake;
+    public int objfound = 0;
     public void GoSearch()
     {
         findthemistake.SetActive(true);
     }
     public void found()
     {
-        findthemistake.SetActive(false);
+        objfound++;
+
+        if (objfound == 3)
+        {
+            objfound = 0;
+            findthemistake.SetActive(false);
+        }
     }
 }
