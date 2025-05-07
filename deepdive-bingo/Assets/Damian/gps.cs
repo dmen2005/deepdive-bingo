e9ae2249607ead32916a8cac6d79feed75c122bf
Mimicall <srhoekstra@st.noorderpoort.nl>
2025-05-07 10:55:02 +0200

    Distance Checking + Location Object

--- deepdive-bingo/Assets/Damian/gps.cs
+++ deepdive-bingo/Assets/Damian/gps.cs
@@ -7,35 +7,29 @@ public class gps : MonoBehaviour
     public tileloader tileloader;
     public float update = 10f;
 
-    public float lattest = 53.2194f;
-    public float longtest = 6.5665f;
+    public double lattest = 53.2132367;
+    public double longtest = 6.5558798;
 
     //all the comments is for when gps is working to lazzy for cleaner solotion rn
 
-    void Start()
+    void Awake()
     {
-
-        if (!Input.location.isEnabledByUser)
+        if (Input.location.isEnabledByUser)
         {
-            Debug.LogError("Location services are not enabled on this device.");
-            return;
+            //Debug.LogError("Location services are not enabled on this device.");
+            Input.location.Start();
         }
-
-        Input.location.Start();
-
 
         InvokeRepeating("UpdateGPS", 0f, update);
     }
 
     void UpdateGPS()
     {
+        Debug.Log("Test");
         if (Input.location.status == LocationServiceStatus.Running)
         {
-            //float latitude = lattest;
-            //float longitude = longtest;
-
-            float latitude = Input.location.lastData.latitude;
-            float longitude = Input.location.lastData.longitude;
+            double latitude = Input.location.lastData.latitude;
+            double longitude = Input.location.lastData.longitude;
 
 
 
@@ -46,6 +40,13 @@ public class gps : MonoBehaviour
         }
         else
         {
+            double latitude = lattest;
+            double longitude = longtest;
+
+            tileloader.latitude = latitude;
+            tileloader.longitude = longitude;
+
+            tileloader.location();
             Debug.LogError("Unable to get GPS data.");
         }
     }
