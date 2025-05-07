e230cd61706f282617a7fe6c82da0b3c98315122
Damian <drentdamian@gmail.com>
2025-05-07 15:20:43 +0200

    really fixed (i hope)

--- deepdive-bingo/Assets/Damian/find_the.....cs
+++ deepdive-bingo/Assets/Damian/find_the.....cs
@@ -4,18 +4,25 @@ using UnityEngine;
 public class find_the: MonoBehaviour
 {
     public GameObject findthemistake;
+    public GameObject butten;
+    public GameObject butten1;
+    public GameObject butten2;
     public int objfound = 0;
     public void GoSearch()
     {
+        butten.SetActive(true);
+        butten1.SetActive(true);
+        butten2.SetActive(true);
+
         findthemistake.SetActive(true);
     }
     public void found()
     {
         objfound++;
-        findthemistake.SetActive(false);
 
         if (objfound == 3)
         {
+            findthemistake.SetActive(false);
             objfound = 0;
         }
     }
